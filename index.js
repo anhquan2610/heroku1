a1bedbefcf8975cfbcca22a73debfa98ea06c85a
anhquan2610 <Quanddagch200326@fpt.edu.vn>
2022-09-22 17:07:58 +0700

    anh quan

---
+++ index.js
@@ -0,0 +1,10 @@
+const { appendFile } = require("fs")
+const http = require("http")
+const port = 3000
+const app = http.createServer((req, res) =>{
+res.end("<h1> Hello  Worrld </h1>")
+res.write("<h3> My Nodejs web page has been deloyed to Heroku successfully!!! </h3> ")
+res.end()
+})
+
+app.listen(port)
