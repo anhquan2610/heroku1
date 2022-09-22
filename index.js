9482f67e1db67aed38675043e88b1df5defee235
anhquan2610 <Quanddagch200326@fpt.edu.vn>
2022-09-22 17:19:31 +0700

    ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { appendFile } = require("fs")
 const http = require("http")
-const port = 3000
+const port = process.env.PORT || 3000
 const app = http.createServer((req, res) =>{
 res.end("<h1> Hello  Worrld </h1>")
 res.write("<h3> My Nodejs web page has been deloyed to Heroku successfully!!! </h3> ")
